ba3c952c1e3e3d2c6f0201780defa018e7b01c21
Miliky <li1063094734@163.com>
2023-04-28 13:38:57 +0800

    init project

---
+++ test/sim_card_info_method_channel_test.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/services.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:sim_card_info/sim_card_info_method_channel.dart';
+
+void main() {
+  MethodChannelSimCardInfo platform = MethodChannelSimCardInfo();
+  const MethodChannel channel = MethodChannel('sim_card_info');
+
+  TestWidgetsFlutterBinding.ensureInitialized();
+
+  setUp(() {
+    channel.setMockMethodCallHandler((MethodCall methodCall) async {
+      return '42';
+    });
+  });
+
+  tearDown(() {
+    channel.setMockMethodCallHandler(null);
+  });
+
+  test('getPlatformVersion', () async {
+    expect(await platform.getPlatformVersion(), '42');
+  });
+}
